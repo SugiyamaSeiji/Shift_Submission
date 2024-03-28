a64c80334dc3ae12b83a4a5dec7e7153e845a776
Seiji <149654566+SugiyamaSeiji@users.noreply.github.com>
2024-03-29 00:43:11 +0900

    Delete app/src/app.py

--- app/src/app.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.dont_write_bytecode = True
-from src import app
-from src.models import employee
-
-if __name__ == '__main__':
-    host = '127.0.0.1'
-    port = 8000
-    app.run(host=host, port=port, debug=True)
