68a2a1c6182e124d4197e87248700622fea3318b
Seiji <149654566+SugiyamaSeiji@users.noreply.github.com>
2024-03-29 15:32:45 +0900

    Update server.py

--- server.py
+++ server.py
@@ -2,8 +2,10 @@ import sys
 sys.dont_write_bytecode = True
 from src import app
 from src.models import employee
+from src import db
 
 if __name__ == '__main__':
+    db.create_all()
     host = '127.0.0.1'
     port = 8888
     app.run(host=host, port=port, debug=True)
