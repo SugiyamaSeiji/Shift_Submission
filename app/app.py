e2155772448ebf2b492a987920be3f3141b35c99
Seiji <149654566+SugiyamaSeiji@users.noreply.github.com>
2024-03-29 12:58:49 +0900

    Delete app directory

--- app/app.py
+++
@@ -1,9 +0,0 @@
-from src.models import employee
-import sys
-sys.dont_write_bytecode = True
-from src import app
-
-if __name__ == '__main__':
-    host = '127.0.0.1'
-    port = 8000
-    app.run(host=host, port=port, debug=True)
